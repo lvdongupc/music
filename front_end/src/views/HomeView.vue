2ca5504fbec132311a28f648b619654e5a25344e
ldd <2567783488@qq.com>
2024-02-24 11:28:25 +0800

    web

---
+++ front_end/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <a href="http://localhost:5173/music">
+    <input type=button value="跳转到音乐列表">
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
